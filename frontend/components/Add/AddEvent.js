0d56447361551de1b2836a9d45c85ca80fa95af7
KikkerKarel <85058961+KikkerKarel@users.noreply.github.com>
2022-07-26 19:30:49 +0200

    working on add

--- frontend/components/Add/AddEvent.js
+++ frontend/components/Add/AddEvent.js
@@ -1,26 +1,83 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import axios from "axios";
 
 export default function AddEvent() {
 
-    const list = ['Title', 'Country', 'Episodes', 'Duration', 'Genres']
+    const list = ['Title', 'Country', 'Episodes', 'Duration', 'Genres'];
+    const [title, changeTitle] = React.useState("");
+    const [country, changeCountry] = React.useState("");
+    const [episodes, changeEpisodes] = React.useState(6);
+    const [duration, changeDuration] = React.useState(0);
+    const [genres, changeGenres] = React.useState([""]);
 
-    const [text, onChangeText] = React.useState("Test text");
+    const json = JSON.stringify({
+        title: title,
+        country: country,
+        episodes: episodes,
+        duration: duration,
+        genres: genres
+    });
+
+    const AddToList = () => {
+        axios.post("/add", json, {headers: { 'Content-Type': 'application/json'}}).then(response => {
+            console.log(response);
+        });
+    }
+
+    // const onChange = (e) => {
+    //     console.log(e.target.getAttribute("data-key"));
+    // }
+
     return (
         <SafeAreaView style={styles.container}>
-            {list.map((item) => {
+            {/* {list.map((item) => {
                 return (
                     <View style={styles.view}>
                         <Text style={styles.text}>{item}</Text>
                         <TextInput
+                            key={item}
                             style={styles.textInput}
                             onChangeText={onChangeText}
+                            onSubmitEditing={onChange}
                             value={text}
                         />
                     </View>
                 )
-            })}
+            })} */}
+            <Text style={styles.text}>Title</Text>
+            <TextInput
+                style={styles.textInput}
+                onChange={changeTitle}
+                value={title}
+            />
+            <Text style={styles.text}>Country</Text>
+            <TextInput
+                style={styles.textInput}
+                onChange={changeCountry}
+                value={country}
+            />
+            <Text style={styles.text}>Episodes</Text>
+            <TextInput
+                style={styles.textInput}
+                onChange={changeEpisodes}
+                value={episodes.toString()}
+            />
+            <Text style={styles.text}>Duration</Text>
+            <TextInput
+                style={styles.textInput}
+                onChange={changeDuration}
+                value={duration.toString()}
+            />
+            <Text style={styles.text}>Genres</Text>
+            <TextInput
+                style={styles.textInput}
+                onChange={changeGenres}
+                value={genres}
+            />
+            <TouchableOpacity style={styles.button} onPress={AddToList}>
 
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
@@ -39,12 +96,13 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         borderRadius: 15,
         height: 30,
+        width: '75%',
         justifyContent: 'center',
     },
     text: {
         color: 'white',
         fontSize: 25,
         fontWeight: 'bold',
-        left: 10
+        // left: 10
     }
 })
